ec2231fc5b5bf6f2eabb1f4e8314e95ec152ee20
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-02-01 19:01:23 +0700

    update nginx with ssl

--- nginx-ssl-production/init-letsencrypt.sh
+++ nginx-ssl-production/init-letsencrypt.sh
@@ -4,7 +4,7 @@ domains=(hoidanitexample.xyz www.hoidanitexample.xyz)
 rsa_key_size=4096
 data_path="./certbot"
 email="abc@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 ## step 1, get fake certificate => run nginx with ssl
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -33,6 +33,47 @@ docker compose run --rm --entrypoint "\
 echo
 
 
-echo "### Starting nginx ..."
+echo "### Starting services, then nginx ..."
+docker compose -p hoidanit up -d mysqldb
+docker compose -p hoidanit up -d backend
 docker compose up --force-recreate -d nginx
 echo
+
+
+# step 2, get real certificate
+echo "### Deleting dummy certificate for $domains ..."
+docker compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
+
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
+
+echo "### Reloading nginx ..."
+docker compose exec nginx nginx -s reload
